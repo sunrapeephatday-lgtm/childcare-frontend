cc8d1b71b6d1855bcb53c493be4bab03575d0cbd
Rapeephat-art <rapeephat.ch@rmuti.ac.th>
2026-05-16 12:51:38 +0700

    erollmentdetailpdf1

--- src/pages/AdminEnrollmentDetail.jsx
+++ src/pages/AdminEnrollmentDetail.jsx
@@ -187,8 +187,8 @@ const childAge = calculateAgeParts(data.birth_date)
 page.drawText(`${bd ? bd.getDate() : "-"}`, { x: 148, y: 690, size, font })
 page.drawText(`${bd ? thaiMonths[bd.getMonth()] : "-"}`, { x: 230, y: 690, size, font })
 page.drawText(`${bd ? bd.getFullYear()+543 : "-"}`, { x: 330, y: 690, size, font })
-page.drawText(`${childAge.years}`, { x: 430, y: 690, size, font })
-page.drawText(`${childAge.months}`, { x: 515, y: 690, size, font })
+page.drawText(`${childAge.years}`, { x: 410, y: 690, size, font })
+page.drawText(`${childAge.months}`, { x: 490, y: 690, size, font })
 /* ===== โรคประจำตัว ===== */
 page.drawText(`${data.congenital_disease || "-"}`, {
   x: 210,
